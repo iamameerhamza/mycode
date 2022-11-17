a75826f1285bd141b61fd4996719e5210dc59d6f
iamameerhamza <112004700+iamameerhamza@users.noreply.github.com>
2022-11-17 12:30:24 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//hamz
+#include <iostream>
+using namespace std;
+ int main(){
+    int A ;
+    float b = 12;
+    char c;
+    cout<< "enter inter:";
+    cin>>A;
+    cout<<"enter chracter:";
+    cin>>c;
+    cout<<A<<endl<<b<<endl<<c;
+}
+
